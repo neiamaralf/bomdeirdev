caa3fc1cb10b876c9024a0b547de10f62a6c0add
Nei Munhoz <neimunhoz@Neis-Mac-mini.local>
2018-09-13 00:54:44 -0300

    Thu Sep 13 00:53:53 -03 2018

--- app/pages/estilos/estilosevt.component.ts
+++ app/pages/estilos/estilosevt.component.ts
@@ -446,6 +446,37 @@ export class EstilosEvtComponent implements OnInit {
 
   }
 
+   WEATHER_ICONS = {
+    day: {
+      'ps': 0xf00d,
+      'n': 0xf002,
+      'drizzle': 0xf009,
+      'rain': 0xf008,
+      'thunderstorm': 0x010,
+      'snow': 0xf00a,
+      'mist': 0xf0b6
+    },
+    night: {
+      'clear': 0xf02e,
+      'clouds': 0xf086,
+      'drizzle': 0xf029,
+      'rain': 0xf028,
+      'thunderstorm': 0xf02d,
+      'snow': 0xf02a,
+      'mist': 0xf04a
+    },
+    neutral: {
+      'temperature': 0xf055,
+      'wind': 0xf050,
+      'cloud': 0xf041,
+      'pressure': 0xf079,
+      'humidity': 0xf07a,
+      'rain': 0xf019,
+      'sunrise': 0xf046,
+      'sunset': 0xf052
+    }
+  };
+
   onclick(item) {
     // this.routerExtensions.navigate(["/item/"+item.id], { clearHistory: false });
     //this.curevento=[];
